5b8a5ba11d51ca3dbb86b0f9f6da9b987463b8c5
mccoy88f <50622699+mccoy88f@users.noreply.github.com>
2025-01-18 22:11:04 +0100

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ const baseConfig = {
     port: process.env.PORT || 10000,
     
     // Content sources
-    M3U_URL: 'https://raw.githubusercontent.com/mccoy88f/OMG-TV-Stremio-Addon/refs/heads/beta/link.playlist',
-    EPG_URL: 'https://raw.githubusercontent.com/mccoy88f/OMG-TV-Stremio-Addon/refs/heads/beta/link.epg',
+    M3U_URL: 'https://raw.githubusercontent.com/mccoy88f/OMG-TV-Stremio-Addon/refs/heads/main/link.playlist',
+    EPG_URL: 'https://raw.githubusercontent.com/mccoy88f/OMG-TV-Stremio-Addon/refs/heads/main/link.epg',
     
     // Feature flags
     enableEPG: true, // EPG attivo di default
