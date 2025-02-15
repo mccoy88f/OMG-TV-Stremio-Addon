d51e5a394422ab59782a700eccf306f84f35b24e
Antonello <50622699+mccoy88f@users.noreply.github.com>
2025-02-15 11:48:00 +0100

    Update handlers.js

--- handlers.js
+++ handlers.js
@@ -25,10 +25,6 @@ async function catalogHandler({ type, id, extra, config: userConfig }) {
        }
 
        const { search, genre, skip = 0 } = extra || {};
-       console.log('Extra params:', extra);
-       console.log('Genre:', genre);
-       console.log('Search:', search);
-       console.log('Skip:', skip);
 
        const cachedData = CacheManager.getCachedData();
        const ITEMS_PER_PAGE = 100;
