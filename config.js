744e33a03c1b84fd6161d82a256f96a51d31da2d
Antonello <50622699+mccoy88f@users.noreply.github.com>
2025-02-15 14:51:31 +0100

    Update config.js

--- config.js
+++ config.js
@@ -3,6 +3,7 @@ const path = require('path');
 
 const baseConfig = {
     port: process.env.PORT || 10000,
+    defaultUserAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36',
     cacheSettings: {
         updateInterval: 12 * 60 * 60 * 1000,
         maxAge: 24 * 60 * 60 * 1000,
