8dcec6a2bba2a1a8bb448cacdc935d0757dd0fd1
Antonello <50622699+mccoy88f@users.noreply.github.com>
2025-02-18 00:23:30 +0100

    Update epg-manager.js

--- epg-manager.js
+++ epg-manager.js
@@ -17,7 +17,7 @@ class EPGManager {
     }
 
     normalizeId(id) {
-        return id?.toLowerCase().replace(/[^\w]/g, '').trim() || '';
+        return id?.toLowerCase().replace(/[^\w.]/g, '').trim() || '';
     }
 
     validateAndSetTimezone() {
