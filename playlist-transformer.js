104e97dd14118cb7a57cb88c34c2adc412d662ed
Antonello <50622699+mccoy88f@users.noreply.github.com>
2025-02-18 00:22:14 +0100

    Update playlist-transformer.js

--- playlist-transformer.js
+++ playlist-transformer.js
@@ -29,7 +29,7 @@ class PlaylistTransformer {
   }
 
   normalizeId(id) {
-      return id?.toLowerCase().replace(/[^\w]/g, '').trim() || '';
+      return id?.toLowerCase().replace(/[^\w.]/g, '').trim() || '';
   }
 
   cleanChannelName(name) {
