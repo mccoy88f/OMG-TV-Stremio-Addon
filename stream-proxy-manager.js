b3f79af2ca971878f726ef025575fe9aa227e2a0
Antonello <50622699+mccoy88f@users.noreply.github.com>
2025-02-24 16:03:52 +0100

    Update stream-proxy-manager.js

--- stream-proxy-manager.js
+++ stream-proxy-manager.js
@@ -213,7 +213,7 @@ class StreamProxyManager {
 
                     return {
                         name: `${streamDetails.name}`,
-                        title: `[P: ${streamType}] ${streamDetails.name}`,
+                        title: `${streamDetails.originalName}\n[Proxy ${streamType}]`,
                         url: proxyUrl,
                         behaviorHints: {
                             notWebReady: false,
