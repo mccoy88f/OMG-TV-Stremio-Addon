4d7155615319394c26eae804e40c1a5504b1eb3a
Antonello <50622699+mccoy88f@users.noreply.github.com>
2025-02-26 21:35:13 +0100

    Update resolver-stream-manager.js

--- resolver-stream-manager.js
+++ resolver-stream-manager.js
@@ -161,7 +161,7 @@ class ResolverStreamManager {
                         console.log(`ℹ️ URL non modificato dal resolver per: ${streamDetails.name}, lo manteniamo`);
                         return {
                             name: `${input.originalName}`,
-                            title: `${streamDetails.name}\n[Flusso Originale]`,
+                            title: `📺 ${streamDetails.name}`,
                             url: streamDetails.url,
                             headers: streamDetails.headers,
                             behaviorHints: {
