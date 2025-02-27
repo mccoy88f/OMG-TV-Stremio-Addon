8f8d0565ab410f07ef3d7aa7527871e0d776ea20
Antonello <50622699+mccoy88f@users.noreply.github.com>
2025-02-27 22:16:14 +0100

    Update views.js

--- views.js
+++ views.js
@@ -304,6 +304,12 @@ const renderConfigPage = (protocol, host, query, manifest) => {
                        <button onclick="backupConfig()">BACKUP CONFIGURAZIONE</button>
                        <input type="file" id="restoreFile" accept=".json" style="display:none;" onchange="restoreConfig(event)">
                        <button onclick="document.getElementById('restoreFile').click()">RIPRISTINA CONFIGURAZIONE</button>
+                       <h4>Ricordati di generare la configurazione prima di eseguire il backup</h4>
+                   </div>
+                   <div style="margin-top: 30px; background: rgba(255,255,255,0.1); padding: 15px; border-radius: 4px;">
+                       <ul style="text-align: center; margin-top: 10px;">
+                           <p>Ricordati di generare la configurazione prima di eseguire il backup</p>
+                       </ul>
                    </div>
                </div>
                
